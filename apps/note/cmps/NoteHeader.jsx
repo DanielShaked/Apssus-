67986c083ea9cedd3ab71a8f1b2221e004020c94
DanielShaked <89588973+DanielShaked@users.noreply.github.com>
2022-01-01 20:19:51 +0200

    fix img bug (pages)

--- apps/note/cmps/NoteHeader.jsx
+++ apps/note/cmps/NoteHeader.jsx
@@ -1,12 +1,12 @@
 import { SearchNote } from "./SearchNote.jsx"
-
+import {} from ''
 
 export function NoteHeader({onSetFilter}) {
     return (
         <div className="note-header ">
             <div className="header main-layout">
             <div className="logo-container ">
-                <img src="../assets/img/keep-logo.png?" alt="" />
+                <img src="../../../assets/img/keep-logo.png" alt="" />
                 <span className="logo-txt">KeepIt </span>
             </div>
                 <SearchNote onSetFilter={onSetFilter}/>
