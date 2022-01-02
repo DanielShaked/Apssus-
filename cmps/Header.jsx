049728d7f6da67f8c868a107a3b469e3f848f7d3
Yuval <yuvalshai95@gmail.com>
2022-01-02 16:05:08 +0200

    refactoring

--- cmps/Header.jsx
+++ cmps/Header.jsx
@@ -1,6 +1,5 @@
-import { HeaderMenu } from "./HeaderMenu.jsx";
-const { NavLink } = ReactRouterDOM
-
+import {HeaderMenu} from './HeaderMenu.jsx';
+const {NavLink} = ReactRouterDOM;
 
 export class Header extends React.Component {
   state = {
@@ -8,52 +7,44 @@ export class Header extends React.Component {
     isOnKeep: false,
   };
 
-    
-    state = {
-        isVisible: false,
-        isOnKeep: false,
-    }
-    
-    
-    
-    onKeep = (isOnKeep) => {
-        this.setState({isOnKeep,})
-    }
+  // state = {
+  //     isVisible: false,
+  //     isOnKeep: false,
+  // }
 
+  onKeep = isOnKeep => {
+    this.setState({isOnKeep});
+  };
 
-    onToggleMenu = () => {
-        const { isVisible } = this.state;
-        this.setState({isVisible: !isVisible})
-    }
+  onToggleMenu = () => {
+    const {isVisible} = this.state;
+    this.setState({isVisible: !isVisible});
+  };
 
-    render() {
-        const { isVisible,isOnKeep } = this.state;
-        
-        return (
-            <div className="header-container">
-                <header className="header main-layout">
-                    <div className="logo-container">
-                        <NavLink exact to="/">
-                            <span onClick={() => this.onKeep(false)} className="logo-txt">Appsus</span>
-                        {(isOnKeep) && <div className="keep-logo"><img src="./assets/img/keep.png" alt="" /></div>}    
-                        </NavLink>
-                        
-                    </div>
-                    
-                {(!isVisible) && <i onClick={this.onToggleMenu} className="fas fa-th-large"></i>  }
-            
-                    {(isVisible) && <HeaderMenu
-                        onToggleMenu={this.onToggleMenu}
-                        onKeep={this.onKeep}/>}
-                </header>
-            </div>
-            
-            )
-        }
-        
-        
+  render() {
+    const {isVisible, isOnKeep} = this.state;
+
+    return (
+      <div className="header-container">
+        <header className="header main-layout">
+          <div className="logo-container">
+            <NavLink exact to="/">
+              <span onClick={() => this.onKeep(false)} className="logo-txt">
+                Appsus
+              </span>
+              {isOnKeep && (
+                <div className="keep-logo">
+                  <img src="./assets/img/keep.png" alt="" />
+                </div>
+              )}
+            </NavLink>
+          </div>
+
+          {!isVisible && <i onClick={this.onToggleMenu} className="fas fa-th-large"></i>}
+
+          {isVisible && <HeaderMenu onToggleMenu={this.onToggleMenu} onKeep={this.onKeep} />}
+        </header>
+      </div>
+    );
+  }
 }
-    
-
-    
-    
