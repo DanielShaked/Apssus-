0cf420a529e703d82f5f4606a810e1d67ab78051
Yuval <yuvalshai95@gmail.com>
2022-01-02 04:21:56 +0200

    refactoring

--- apps/note/cmps/NoteEditor.jsx
+++ apps/note/cmps/NoteEditor.jsx
@@ -1,43 +1,41 @@
-import { ColorPicker } from "./ColorPicker.jsx";
-
+import {ColorPicker} from './ColorPicker.jsx';
 
 export class NoteEditor extends React.Component {
-    state = {
-        isColorPickerOpen: false,
-    }
+  state = {
+    isColorPickerOpen: false,
+  };
 
-    
-    onRemoveNote = () => {
-        this.props.removeNote(this.props.note.id);
-        // TODO: busService user-msg
-    }
-    
-    onDuplicateNote = () => {
-        const { duplicateNote, note } = this.props;
-        duplicateNote(note.id);
-    }
-    
-    onToggleColorPicker = (isOpen) => {
-        this.setState({isColorPickerOpen: isOpen})
-    }
-    
-    render() {
-        const { note, changeBgColor} = this.props;
-        const { isColorPickerOpen } = this.state;
-        console.log('isColorPickerOpen:', isColorPickerOpen);
-        
-        return(
-            <section className="note-editor">
-                <i onClick={this.onDuplicateNote} className="fas fa-paste"></i>
-                <i onClick={() => this.onToggleColorPicker(true)} className="fas fa-palette"></i>
-                {(isColorPickerOpen)
-                    && <ColorPicker
-                        changeBgColor={changeBgColor}
-                        onToggleColorPicker={this.onToggleColorPicker}
-                        id={note.id} />}
-                <i onClick={this.onRemoveNote} className="far fa-trash-alt"></i>
-            </section>
-        )
-    }
+  onRemoveNote = () => {
+    this.props.removeNote(this.props.note.id);
+    // TODO: busService user-msg
+  };
 
+  onDuplicateNote = () => {
+    const {duplicateNote, note} = this.props;
+    duplicateNote(note.id);
+  };
+
+  onToggleColorPicker = isOpen => {
+    this.setState({isColorPickerOpen: isOpen});
+  };
+
+  render() {
+    const {note, changeBgColor} = this.props;
+    const {isColorPickerOpen} = this.state;
+
+    return (
+      <section className="note-editor">
+        <i onClick={this.onDuplicateNote} className="fas fa-paste"></i>
+        <i onClick={() => this.onToggleColorPicker(true)} className="fas fa-palette"></i>
+        {isColorPickerOpen && (
+          <ColorPicker
+            changeBgColor={changeBgColor}
+            onToggleColorPicker={this.onToggleColorPicker}
+            id={note.id}
+          />
+        )}
+        <i onClick={this.onRemoveNote} className="far fa-trash-alt"></i>
+      </section>
+    );
+  }
 }
